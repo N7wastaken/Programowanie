308f29b3f91d82cda23598b9cab757f8a53ea7be
N7wastaken <76101301+N7wastaken@users.noreply.github.com>
2023-05-09 11:37:16 +0200

    Add files via upload

---
+++ C++/C++/niewiem.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+int main(){
+	
+			cout<<"Wpisz 1 aby wlaczyc program ";
+			cout<<endl;
+	
+		
+	int wybor;
+	
+		cin>>wybor;
+		
+		if(wybor==1){
+	
+this.mehButton1.BackColor = System.Drawing.SystemColors.Control;
+
+		}
+	
+
+		
+		else{
+			cout<<"Error";
+		}
+		return 0;
+}
