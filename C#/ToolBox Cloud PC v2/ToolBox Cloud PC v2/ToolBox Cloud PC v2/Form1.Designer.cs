8be8aebbddf3e55420fa5f7603f5465b80bc81b1
N7wastaken <76101301+N7wastaken@users.noreply.github.com>
2023-05-09 11:33:18 +0200

    Add files via upload

---
+++ C#/ToolBox Cloud PC v2/ToolBox Cloud PC v2/ToolBox Cloud PC v2/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace ToolBox_Cloud_PC_v2
+{
+    partial class CloudPCToolBox
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CloudPCToolBox));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(39, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 198);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Intranet";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(675, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 152);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ServiceDesk";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(240, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 155);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Cloud Disk ";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(495, 258);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(172, 153);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Zabbix";
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(892, 64);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(129, 169);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Teta";
+            this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.Location = new System.Drawing.Point(204, 49);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(302, 209);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Security Portal";
+            this.button8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button10
+            // 
+            this.button10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button10.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button10.Image = ((System.Drawing.Image)(resources.GetObject("button10.Image")));
+            this.button10.Location = new System.Drawing.Point(692, 263);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(194, 148);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Messenger";
+            this.button10.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button11.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button11.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button11.Image = ((System.Drawing.Image)(resources.GetObject("button11.Image")));
+            this.button11.Location = new System.Drawing.Point(35, 253);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(190, 165);
+            this.button11.TabIndex = 9;
+            this.button11.Text = "SW Drive";
+            this.button11.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button12.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button12.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button12.Image = ((System.Drawing.Image)(resources.GetObject("button12.Image")));
+            this.button12.Location = new System.Drawing.Point(495, 59);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(174, 178);
+            this.button12.TabIndex = 8;
+            this.button12.Text = "Mail";
+            this.button12.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // CloudPCToolBox
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1079, 466);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "CloudPCToolBox";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cloud PC ToolBox ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ C#/ToolBox Cloud PC v2/ToolBox Cloud PC v2/ToolBox Cloud PC v2/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Configuration;
+
+namespace ToolBox_Cloud_PC_v2
+{
+    public partial class CloudPCToolBox : Form
+    {
+        public CloudPCToolBox()
+        {
+            InitializeComponent();
+        }
+        //
+        // Intranet
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo cmd = new ProcessStartInfo();
+            cmd.FileName = @"cmd.exe";
+            cmd.WindowStyle= ProcessWindowStyle.Hidden;
+            cmd.Arguments = @"/c start https://lgema.lge.com/index.php?action=main_page";
+            Process.Start(cmd);
+        }
+        //
+        // Security Portal
+        //
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo s = new ProcessStartInfo();
+            s.FileName = @"cmd.exe";
+            s.WindowStyle = ProcessWindowStyle.Hidden;
+            s.Arguments = @"/c start http://security.lge.com:5030/login.LoginDo.public.laf";
+            Process.Start(s);
+        }
+        //
+        // Mail
+        //
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo m = new ProcessStartInfo();
+            m.FileName = @"cmd.exe";
+            m.WindowStyle = ProcessWindowStyle.Hidden;
+            m.Arguments = @"/c start http://sso.lge.com";
+            Process.Start(m);
+        }
+        //
+        // ServiceDesk
+        //
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo Serv = new ProcessStartInfo();
+            Serv.FileName = @"cmd.exe";
+            Serv.WindowStyle = ProcessWindowStyle.Hidden;
+            Serv.Arguments = @"/c start https://lgema.lge.com/index.php?action=login";
+            Process.Start(Serv);
+        }
+        //
+        // Teta
+        //
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo Tet = new ProcessStartInfo();
+            Tet.FileName = @"cmd.exe";
+            Tet.WindowStyle = ProcessWindowStyle.Hidden;
+            Tet.Arguments = @"/c start https://ma-mf10-teta.lge.net/Core/Account/Login?ReturnUrl=%2F";
+            Process.Start(Tet);
+        }
+        //
+        // +SW dysk >> SW
+        //
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo SW = new ProcessStartInfo();
+            SW.FileName = @"cmd.exe";
+            SW.WindowStyle = ProcessWindowStyle.Hidden;
+            SW.Arguments = @"/c cd C:\\Users\\arkadiusz.mulkityn\\Desktop\\ToolBox Cloud PC v2 && start SW.bat";
+            Process.Start(SW);
+
+            Thread.Sleep(3000); // pauza na 3 sekundy
+
+            Process.Start(@"S:");
+        }
+       //
+       // Cloud Disk >> Planning
+       //
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo Di = new ProcessStartInfo();
+            Di.FileName = @"cmd.exe";
+            Di.WindowStyle = ProcessWindowStyle.Hidden;
+            Di.Arguments = @"/c cd C:\Program Files (x86)\LG CNS\CloudDisk\ && start LGCNS.CDS.UpdateManager.exe";
+            Process.Start(Di);
+
+            Thread.Sleep(3000); // pauza na 3 sekundy
+
+            Process.Start(@"Z:\01. Planning");
+        }
+        //
+        // zabbix
+        //
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo za = new ProcessStartInfo();
+            za.FileName = @"cmd.exe";
+            za.WindowStyle = ProcessWindowStyle.Hidden;
+            za.Arguments = @"/c start http://150.150.222.42/zabbix/zabbix.php?action=dashboard.view";
+            Process.Start(za);
+        }
+        //
+        // Messenger
+        //
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo mes = new ProcessStartInfo();
+            mes.FileName = @"cmd.exe";
+            mes.WindowStyle = ProcessWindowStyle.Hidden;
+            mes.Arguments = @"/c cd C:\Program Files (x86)\LG UCAP\ && start UCAP.exe";
+            Process.Start(mes);
+        }
+        
+     
+    }
+}
